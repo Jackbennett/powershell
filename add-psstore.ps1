91ff618f681cf55d86cbfd2b6b7cdcd66892c944
Jack Bennett <me@JackBen.net>
2013-10-10 16:05:25 +0100

    add-psstore: comparator doens't seem to like a backslash in the string

--- add-psstore.ps1
+++ add-psstore.ps1
@@ -1,6 +1,6 @@
 New-PSDrive -Name psStore -PSProvider FileSystem -Root \\uhserver1\psstore -Description "Powershell script and module store"
 
-if($env:PSModulePath -notmatch ';psStore:\')
+if($env:PSModulePath -notmatch ';psStore:')
 {
     $env:PSModulePath += ';psStore:\'
 }
