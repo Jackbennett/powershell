8e8635ff7d2e49c723aad4173b086c4f38e370c8
Jack Bennett <me@JackBen.net>
2013-10-10 14:54:55 +0100

    cleaning out some old useless 1 liners pretending to be a script

--- get-installedWindowsFeatures.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-Get-WindowsFeature | 
-where {$_.installed -eq 'false'} | 
-Select-Object installed,displayNAme,name | 
-ConvertTo-Json | 
-out-file c:\test.json
--- list-printUsers.ps1
+++
@@ -1,3 +0,0 @@
-﻿Get-ADUser -Filter * -SearchBase "ou=Student Teachers" -Properties username
-
-Get-ADUser -Filter * -SearchScope "ou=Staff" -Properties username
