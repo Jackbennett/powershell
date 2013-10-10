fc6f74c647005c56ad2294cb03d933ac0aa3de9d
Jack Bennett <me@JackBen.net>
2013-10-10 15:56:50 +0100

    [add-psStore] test if the psStore is in the $env:PSModulePath list and if not, append it

--- add-psstore.ps1
+++ add-psstore.ps1
@@ -1,1 +1,6 @@
 New-PSDrive -Name psStore -PSProvider FileSystem -Root \\uhserver1\psstore -Description "Powershell script and module store"
+
+if($env:PSModulePath -notmatch ';psStore:\')
+{
+    $env:PSModulePath += ';psStore:\'
+}
