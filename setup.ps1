28847cc330d27cd9c40c99add910916299756ff0
Jack Bennett <me@JackBen.net>
2013-10-11 11:19:17 +0100

    Make setup.ps1 into a script that's safe to run all the time.

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,34 @@
-New-PSDrive -Name psStore -PSProvider FileSystem -Root \\uhserver1\psstore -Description "Powershell script and module store"
 
-if($env:PSModulePath -notmatch ';psStore:')
+Param(
+    # What name would you prefer the drive to be mapped under.
+    [string]$DriveName = "psStore"
+
+    , # If you don't have an existing profile, Copy our template in that maps the drive as your profile.
+    [switch]$copyProfile
+)
+
+
+# Add the network path to the shared powershell scripts as a drive for the current user
+New-PSDrive -Name $DriveName -PSProvider FileSystem -Root \\uhserver1\psstore -Description "Powershell script and module store"
+
+
+# Check to see if the above added drive is in the module path. If not, add it.
+if($env:PSModulePath -notmatch "$DriveName`:")
 {
-    $env:PSModulePath += ';psStore:\'
+    $env:PSModulePath += ";$DriveName`:\"
+}
+
+if ($copyProfile)
+{
+    if( (test-path $profile.CurrentUserAllHosts) )
+    {
+        Write-output ("Will not overwrite existing profile in: " + $profile.CurrentUserAllHosts)
+    }
+    else
+    {
+        new-item (split-path $profile.CurrentUserAllHosts) -ItemType Directory 
+        Copy-Item "$DriveName1`:\profile.template" $profile.CurrentUserAllHosts
+    }
 }
 
 
