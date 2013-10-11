6cdd8f5e5baf1619ab8a08bcbb994432bb25bbea
Jack Bennett <me@JackBen.net>
2013-10-11 10:30:20 +0100

    removing the redundant import-all script. adding to the module path imports it anway. note: commented copy of the function is in this add-psStore script

--- add-psstore.ps1
+++ add-psstore.ps1
@@ -4,3 +4,13 @@ if($env:PSModulePath -notmatch ';psStore:')
 {
     $env:PSModulePath += ';psStore:\'
 }
+
+
+<#
+ # I don't think this is nescsary once you've added the psstore to the module path
+#>
+    <#
+    function Import-All($alias){
+        Get-ChildItem -Path psstore:\ -Directory | select name | Import-Module $_
+    }
+    #>
--- import-all.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem -Path $PSScriptRoot\*.psm1 -Recurse | Foreach-Object{ import-module $_.FullName }
-
-
-. \\uhserver1\psstore\add-psstore.ps1
