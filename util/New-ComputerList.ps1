589beec3a7459b05e30ca28886d56a7ca6efd72b
Jack Bennett <me@JackBen.net>
2013-10-18 10:32:39 +0100

    beginning work on splitting the computer name generation from flowol mimics into its own utility module

---
+++ util/New-ComputerList.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.Synopsis
+   Create a computer list given a room name and range value
+.DESCRIPTION
+   Will create a computer list given a room name and range value without verifying if these names exist in the network.
+
+   Pipeline output is an object with properties computerName
+
+.EXAMPLE
+   new-computerList -roomName 50 -range 8..11
+
+   Output: 50-08, 50-09, 50-10, 50-11
+
+.EXAMPLE
+    new-computerList 40 -range 2,3,4,10 | copy-mimics
+#>
+function new-computerList
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Param1,
+
+        # Param2 help description
+        [int]
+        $Param2
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+    }
+    End
+    {
+    }
+}
