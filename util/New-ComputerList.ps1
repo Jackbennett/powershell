821051bebd4cb02fb6d167efc4b805dbe9cfe0c3
Jack Bennett <me@JackBen.net>
2013-10-24 14:18:45 +0100

    Working computerlist

--- util/New-ComputerList.ps1
+++ util/New-ComputerList.ps1
@@ -33,26 +33,43 @@ function new-computerList
         [int[]]
         $Range = 1
 
+        , #Add Full Qualified Domain name
+        [switch]
+        $FQDN = $false
     )
 
     Begin
     {
         # Create a list to add computer names to
-        $computerName = New-Object Collections.Generic.List[string]
+        $computerName = New-Object System.Collections.Generic.List[string]
+
+        #What's the fully qualified domain name in case we need it
+            $domain = [ADSI]"LDAP://RootDSE"
+        $domain = $domain.rootDomainNamingcontext -replace ",?DC=", "."
     }
 
     Process
     {
         # We need to build the string ROOM-COMPUTER
         
-        #So for each room we've been given
+        # So for each room we've been given
         foreach($r in $room){
+            Write-Verbose "Making list for Room: $r"
 
-            #Make a computer for each of the ranges specified
+            # Make a computer for each of the ranges specified
             foreach($c in $range){
+                Write-Verbose "adding computer: $c to the room list"
+                # Add this string to our list of computernames with a fully qualified domain name
+                $computerName.Add("$r-$($c.toString('00'))")
+            }
+        }
 
-                #Add this string to our list of computernames with a fully qualified domain name
-                $computerName.Add("$r-$($c.toString('00')).upholland.lancsngfl.ac.uk")
+        if($FQDN)
+        {
+            Write-Verbose ('Adding FQDN [' + $domain + '] to all ' + $computerName.Count + ' computer names')
+            for($i = 0; $i -lt $computerName.Count; ++$i)
+            {
+                $computerName[$i] += $domain
             }
         }
     }
@@ -60,7 +77,7 @@ function new-computerList
     End
     {
         # Return the list of computer names to the pipeline
-        Write-Verbose "Returning to the pipeline with: $computername"
+        Write-Verbose "Returning the list to pipeline"
 
         return $computerName
     }
