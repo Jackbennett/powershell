32c2be3ef8019685b7a37dd059f7819aeca4fac2
Jack Bennett <me@jackben.net>
2013-11-22 23:21:41 +0000

    cleaning up the cmdlet and adding some help to explain the goal

--- Get-SerialPort.ps1
+++ Get-SerialPort.ps1
@@ -1,62 +1,18 @@
 ﻿<#
 .Synopsis
-   Short description
+   Print a line of whatever data comes int he Serial Port. Read Only
 .DESCRIPTION
-   Long description
-.EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
-.INPUTS
-   Inputs to this cmdlet (if any)
-.OUTPUTS
-   Output from this cmdlet (if any)
-.NOTES
-   General notes
-.COMPONENT
-   The component this cmdlet belongs to
-.ROLE
-   The role this cmdlet belongs to
-.FUNCTIONALITY
-   The functionality that best describes this cmdlet
+   Echo all incoming data on the serial port. I want to wrap this with node and use this as a dev module instead of installing VS 2013 to just compile nodeserialport
 #>
 function Get-SerialPort
 {
-    [CmdletBinding(DefaultParameterSetName='Parameter Set 1', 
-                  SupportsShouldProcess=$true, 
-                  PositionalBinding=$false,
-                  HelpUri = 'http://www.microsoft.com/',
-                  ConfirmImpact='Medium')]
+    [CmdletBinding()]
     [OutputType([String])]
     Param
     (
-        # Param1 help description
-        [Parameter(ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true, 
-                   ValueFromRemainingArguments=$false, 
-                   Position=0,
-                   ParameterSetName='Parameter Set 1')]
-        [ValidateNotNull()]
-        [ValidateNotNullOrEmpty()]
-        [ValidateCount(0,5)]
-        [ValidateSet("sun", "moon", "earth")]
-        [Alias("p1")] 
-        $Param1,
-
-        # Param2 help description
-        [Parameter(ParameterSetName='Parameter Set 1')]
-        [AllowNull()]
-        [AllowEmptyCollection()]
-        [AllowEmptyString()]
-        [ValidateScript({$true})]
-        [ValidateRange(0,5)]
-        [int]
-        $Param2,
-
         # Event Identifier
         [Parameter(ParameterSetName='Event Identifier')]
         [ValidatePattern("[a-z]*")]
-        [ValidateLength(0,15)]
         [String]
         $identifier = "Serial"
     )
@@ -64,51 +20,39 @@ function Get-SerialPort
     Begin
     {
         $port = New-Object system.io.ports.serialport COM3,9600,none,8,one;
-        $port.ReadTimeout = 4000; # 2 seconds
+        $port.ReadTimeout = 4000; # 4 seconds
         $port.open();
     }
     Process
     {
+        $job = Register-ObjectEvent `
+                -InputObject $port `
+                -EventName "DataReceived" `
+                -SourceIdentifier $identifier `
+                -MessageData 5 `
+                -Action {
+                    $Script:counter += 1
+
+                    Write-Host "Event count: $counter"
+                    write-host $sender.ReadExisting()
+
+                    if($counter -ge $event.messageData)
+                    {
+                        Write-Host "Remove Event:" $event.SourceIdentifier
+                        Unregister-Event $event.SourceIdentifier
+                    }
+                }
+
         try
         {
-            if ($pscmdlet.ShouldProcess("Target", "Operation"))
-            {
-                $job = Register-ObjectEvent `
-                            -InputObject $port `
-                            -EventName "DataReceived" `
-                            -SourceIdentifier $identifier `
-                            -MessageData 5 `
-                            -Action {
-                                $Script:counter += 1
-                                Write-Host "Event count: $counter"
-
-                                if($counter -ge $event.messageData)
-                                {
-                                    Write-Host "Stop Event"
-                                    # exit
-                                }
-
-                                write-host $sender.ReadExisting()
-                            }
-
-                Wait-Event $identifier
-
-                #Receive-Job $identifier
-
-                # $port.ReadTo("sta1234rt   , ");
-            }
+            Wait-Event $identifier
         } finally {
             $port.Close();
             $port.Dispose();
-
             Unregister-Event $identifier
         }
     }
     End
     {
-        $port.Close();
-        $port.Dispose();
-
-        Unregister-Event $identifier
     }
 }
