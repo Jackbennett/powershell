c2e93fe0849598ae662ff825d8c5a6ed72e55842
Jack Bennett <me@jackben.net>
2013-11-23 00:48:22 +0000

    it looks a lot neater but pretty sure this doesn't work at all now

--- Get-SerialPort.ps1
+++ Get-SerialPort.ps1
@@ -23,38 +23,36 @@ function Get-SerialPort
     Begin
     {
         $port = New-Object system.io.ports.serialport COM3,9600,none,8,one
-        $port.ReadTimeout = 4000 # 4 seconds
+        $port.ReadTimeout = 1000 # milliseconds
         $port.open()
+
+        # Print data only when recieved, No polling the port that's crazy talk.
+        Register-ObjectEvent `
+                -InputObject $port `
+                -EventName "DataReceived" `
+                -SourceIdentifier $identifier
     }
     Process
     {
-        # Print data only when recieved, No polling the port that's crazy talk.
-        $job = Register-ObjectEvent `
-                -InputObject $port `
-                -EventName "DataReceived" `
-                -SourceIdentifier $identifier `
-                -MessageData 5 `
-                -Action {
-                    $Script:counter += 1
-
-                    Write-Host "Event count: $counter"
-                    # Dump the entire buffer
-                    write-host $sender.ReadExisting()
-
-                    # Debug counter to stop spewing 400+ events while I close the hung process and burn my lap.
-                    if($counter -ge $event.messageData)
-                    {
-                        Write-Host "Remove Event:" $event.SourceIdentifier
-                        Unregister-Event $event.SourceIdentifier
-                    }
-                }
-
         try
         {
-            Wait-Event $identifier
-            # At this point I think we wait forever as we've unbound the event after 5 mesages.
-            # I cannot cancel the process with ctrl-c. - Not desired
+            Wait-Event $identifier -Timeout ($port.ReadTimeout/1000) -ErrorAction Stop
+            "data or timeout?"
+            $port.ReadExisting()
+
+        # How do I catch the timeout, it aint the following;
+        } catch [System.Management.Automation.PSEventArgs]{
+            "hey caught a timeout"
+
+        # How do I catch success?
+        } catch {
+            "Cool, Got some data"
+            $counter += 1
+            "Event count: $counter"
+            # Dump the entire buffer
+            $port.ReadExisting()
         } finally {
+            "Clean up connections"
             # Stop Listening to data
             $port.Close()
             # Free the resources just to be sure it's safely unbound for sucessive runs of the script
