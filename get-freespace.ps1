5b07b81729376405a5ba5f824a72ada4960d5a3c
Jack Bennett <me@jackben.net>
2013-12-11 12:48:28 +0000

    making the freespace finder get data back about all local drives on some sample machines

--- get-freespace.ps1
+++ get-freespace.ps1
@@ -1,5 +1,29 @@
-$disk = Get-WmiObject Win32_LogicalDisk -ComputerName remotecomputer -Filter "DeviceID='C:'" |
-Select-Object Size,FreeSpace
+$FreeSpace = @{
+    name="FreeSpace/gb"
+    expression={
+        [Math]::Round($_.freeSpace/1gb, 2)
+    }
+}
 
-$disk.Size
-$disk.FreeSpace
+$Size = @{
+    name="Size/gb"
+    expression={
+        [Math]::Round($_.Size/1gb, 2)
+    }
+}
+$computerName = @{
+    name="Computer Name"
+    expression={
+        $_.__SERVER
+    }
+}
+
+Get-WmiObject `
+    -class win32_logicaldisk `
+    -ComputerName 26-19,25-16 `
+    |
+    select `
+        $computerName,
+        deviceid,
+        $FreeSpace,
+        $Size
