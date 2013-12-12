62de5a05a58db9d3af8e485c6b24ea2d523e064b
Jack Bennett <me@jackben.net>
2013-12-12 11:07:39 +0000

    polished pipelinable freespace finder. can't use a WMI query as it does not return the psComputername/__SERVER property

--- get-freespace.ps1
+++ get-freespace.ps1
@@ -1,32 +1,61 @@
-$FreeSpace = @{
-    name="FreeSpace (GB)"
-    expression={
-        [Math]::Round($_.freeSpace/1gb, 2)
-    }
-}
+<#
+.Synopsis
+   List free space and total sizes on selected machines.
+.DESCRIPTION
+   List free space and total sizes on selected machines.
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-FreeSpace
+{
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    Param
+    (
+        # Target computer names
+        [Parameter(ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $ComputerName = 'localhost'
+    )
 
-$Size = @{
-    name="Size (GB)"
-    expression={
-        [Math]::Round($_.Size/1gb, 2)
+    Begin
+    {
     }
-}
-$computerName = @{
-    name="Computer Name"
-    expression={
-        $_.__SERVER
-    }
-}
+    Process
+    {
+        $FreeSpace = @{
+            name="FreeSpace (GB)"
+            expression={
+                [Math]::Round($_.freeSpace/1gb, 2)
+            }
+        }
 
-Get-WmiObject `
-    -Class win32_logicaldisk `
-    -ComputerName 26-19,25-16 `
-    -ErrorAction SilentlyContinue `
-    |
-    select `
-        $computerName,
-        deviceid,
-        $FreeSpace,
-        $Size `
-    |
-    Write-Output
+        $Size = @{
+            name="Size (GB)"
+            expression={
+                [Math]::Round($_.Size/1gb, 2)
+            }
+        }
+        $SourceComputer = @{
+            name="Computer Name"
+            expression={
+                $_.__SERVER
+            }
+        }
+
+        Get-WmiObject `
+            -Class win32_logicaldisk `
+            -ComputerName $ComputerName `
+            -ErrorAction SilentlyContinue `
+            |
+            select `
+                $SourceComputer,
+                deviceid,
+                $FreeSpace,
+                $Size `
+                |
+                Write-Output
+    }
+}
