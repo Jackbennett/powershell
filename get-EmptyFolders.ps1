64b79c447d8b66589316ea01bd15f76ab3d8c40b
Jack Bennett <me@jackben.net>
2013-12-12 10:41:32 +0000

    Yep. That the end of those year 11s home folders. Adding permissions reminder warning and wait

--- get-EmptyFolders.ps1
+++ get-EmptyFolders.ps1
@@ -1,1 +1,5 @@
+Write-Warning "Make sure you own permissions to read the contents of all the directories" 
+
+Pause
+
 ls | ?{$_.PSIsContainer -eq $true} | ? {$_.getFiles().count -eq0 } | select @{name="Path";expression={$_.fullname}}
