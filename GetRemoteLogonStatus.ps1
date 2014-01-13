6b802398909cb364251421f9a604364b2fd8a4cf
Jack Bennett <me@jackben.net>
2014-01-13 13:09:58 +0000

    From MS Scriptcenter | detect if a target computer is locked

---
+++ GetRemoteLogonStatus.ps1
@@ -0,0 +1,24 @@
+﻿# This function will return the logged-on status of a local or remote computer
+# Written by BigTeddy 10 September 2012
+# Version 1.0
+# Sample usage:
+# GetRemoteLogonStatus '<remoteComputerName>'
+
+function GetRemoteLogonStatus ($computer = 'localhost') {
+if (Test-Connection $computer -Count 2 -Quiet) {
+    try {
+        $user = $null
+        $user = gwmi -Class win32_computersystem -ComputerName $computer | select -ExpandProperty username -ErrorAction Stop
+        }
+    catch { "Not logged on"; return }
+    try {
+        if ((Get-Process logonui -ComputerName $computer -ErrorAction Stop) -and ($user)) {
+            "Workstation locked by $user"
+            }
+        }
+    catch { if ($user) { "$user logged on" } }
+    }
+else { "$computer Offline" }
+}
+
+
