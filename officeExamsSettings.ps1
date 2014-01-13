e04886cbfcd7b06f3a3be0dede1ddbe1d34a6d96
Jack Bennett <me@jackben.net>
2014-01-13 13:07:26 +0000

    Script with functions to remove and add the spelling and grammar checking protions of words settings; ToDO: amend for all office applications not just word

---
+++ officeExamsSettings.ps1
@@ -0,0 +1,32 @@
+﻿
+function Set-OfficeRestrictions
+{
+    $word = New-Object -ComObject word.application
+
+    $options = @{
+        'checkSpellingAsYouType' = $false;
+        'checkGrammarAsYouType' = $false;
+        'SuggestSpellingCorrections' = $false;
+        'ContextualSpeller' = $false;
+    }
+
+    $options.keys | foreach{ $word.options.($_) = $myoptions.($_) }
+
+    $word.quit()
+}
+
+function Remove-OfficeRestrictions
+{
+    $word = New-Object -ComObject word.application
+
+    $options = @{
+        'checkSpellingAsYouType' = $true;
+        'checkGrammarAsYouType' = $true;
+        'SuggestSpellingCorrections' = $true;
+        'ContextualSpeller' = $true;
+    }
+
+    $options.keys | foreach{ $word.options.($_) = $myoptions.($_) }
+
+    $word.quit()
+}
