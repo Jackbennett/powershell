8e8c72151f2a5b501f0f04b8c52598b94220c5c7
Jack Bennett <me@jackben.net>
2014-01-14 14:40:14 +0000

    Splitting the script to work as a log-off and log-on

---
+++ Set-OfficeRestrictions.ps1
@@ -0,0 +1,56 @@
+﻿# Options only seem to exist for Word.
+[string[]]$Options = @(
+    'checkSpellingAsYouType',
+    'checkGrammarAsYouType',
+    'SuggestSpellingCorrections',
+    'ContextualSpeller'
+)
+[string[]]$ActiveDocument = @(
+    'ShowSpellingErrors',
+    'ShowGrammaticalErrors'
+)
+
+function Set-OfficeRestrictions
+{
+    Begin
+    {
+        # Detect if word is already open
+        try
+        {
+            $open = Get-Process -Name winword -ErrorAction Stop
+        }
+        catch
+        {
+            $open = $false
+        }
+
+        $Word = New-Object -ComObject word.application
+    }
+
+    Process
+    {
+        $Options | ForEach-Object{
+            $Word.Options.($_) = $false
+        }
+        
+        if($Word.ActiveDocument)
+        {
+            $ActiveDocument | ForEach-Object{
+                $Word.ActiveDocument.($_) = $false
+            }
+        }
+    }
+
+    End
+    {
+        # Close Word if not already open
+        if( -not $open)
+        {
+            $Word.Quit()
+            # To release the ComObject, <object>.Quit() must have been run
+            [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Word) > $null
+        }
+
+        Remove-Variable Word
+    }
+}
