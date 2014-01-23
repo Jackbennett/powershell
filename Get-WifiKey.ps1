88339ea3e5ca9ec79b07e60dd24f1b59e9c98a0f
Jack Bennett <me@jackben.net>
2014-01-23 16:24:10 +0000

    Starting work on a wifi guest password finder script for the campus

---
+++ Get-WifiKey.ps1
@@ -0,0 +1,13 @@
+﻿$Input = 'september'
+
+$Algorithm = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+
+$Text = New-Object -TypeName System.Text.UTF8Encoding
+
+$Key = [System.BitConverter]::ToString(
+    $Algorithm.ComputeHash(
+        $text.GetBytes( $Input )
+        )
+    ).replace('-','').remove(9).ToLower()
+
+$Key
