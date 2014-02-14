3e1ed7ff2f4812c3d8d477fe51c0a30b5af47525
Jack Bennett <me@jackben.net>
2014-02-14 13:30:10 +0000

    Small monitor for the print servers

---
+++ Watch-PrintServer.ps1
@@ -0,0 +1,35 @@
+﻿function Watch-PrintServer
+{
+    $s = get-service spooler -computername vprint-01 | select MachineName,status
+
+    write-host -Object "$($s.MachineName): " -NoNewline
+
+    if($s.status -like 'Running'){
+        Write-Host -BackgroundColor Green -Object $s.status -NoNewline 
+    } else {
+        Write-Host -BackgroundColor Red -Object $s.status -NoNewline
+    }
+
+    $s = get-service spooler -computername vprint-02 | select MachineName,status
+
+    write-host -Object " - $($s.MachineName): " -NoNewline
+
+    if($s.status -like 'Running'){
+        Write-Host -BackgroundColor Green -Object $s.status -NoNewline 
+    } else {
+        Write-Host -BackgroundColor Red -Object $s.status -NoNewline
+    }
+
+    $s = get-service spooler -computername vprint-03 | select MachineName,status
+
+    write-host -Object " - $($s.MachineName): " -NoNewline
+
+    if($s.status -like 'Running'){
+        Write-Host -BackgroundColor Green -Object $s.status 
+    } else {
+        Write-Host -BackgroundColor Red -Object $s.status
+    }
+
+    sleep -Seconds 5
+    check
+}
