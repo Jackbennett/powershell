0f85bf973a40600a195353e06ee3022deca071ab
Jack Bennett <me@jackben.net>
2014-03-26 10:21:31 +0000

    Removing Server 1 from the check

--- Watch-PrintServer.ps1
+++ Watch-PrintServer.ps1
@@ -1,34 +1,31 @@
 ﻿function Watch-PrintServer
 {
-    $s = get-service spooler -computername vprint-01 | select MachineName,status
+    # Check vPrint-2
+    $s = get-service spooler -computername vPrint-02 | select MachineName,status
 
     write-host -Object "$($s.MachineName): " -NoNewline
 
     if($s.status -like 'Running'){
-        Write-Host -BackgroundColor Green -Object $s.status -NoNewline 
+        Write-Host -BackgroundColor DarkGreen -Object $s.status -NoNewline
     } else {
         Write-Host -BackgroundColor Red -Object $s.status -NoNewline
     }
 
-    $s = get-service spooler -computername vprint-02 | select MachineName,status
 
-    write-host -Object " - $($s.MachineName): " -NoNewline
+    write-host -Object " - " -NoNewline
+
+    # Chack vPrint-03
+    $s = get-service spooler -computername vPrint-03 | select MachineName,status
+
+    write-host -Object "$($s.MachineName): " -NoNewline
 
     if($s.status -like 'Running'){
-        Write-Host -BackgroundColor Green -Object $s.status -NoNewline 
+        Write-Host -BackgroundColor DarkGreen -Object $s.status -NoNewline
     } else {
         Write-Host -BackgroundColor Red -Object $s.status -NoNewline
     }
 
-    $s = get-service spooler -computername vprint-03 | select MachineName,status
 
-    write-host -Object " - $($s.MachineName): " -NoNewline
-
-    if($s.status -like 'Running'){
-        Write-Host -BackgroundColor Green -Object $s.status 
-    } else {
-        Write-Host -BackgroundColor Red -Object $s.status
-    }
 
     sleep -Seconds 5
     Watch-PrintServer
