f433db9078c10e6d5c9ba0ebc425a1bbef8b3a34
Jack Bennett <me@jackben.net>
2014-05-06 15:32:44 +0100

    a record of slightly un-common one line commands i've had to work out how to make.

---
+++ oneliners.ps1
@@ -0,0 +1,9 @@
+# Never let this script run alone
+exit 1
+
+# .Remove all scripts for users logon on the domain.
+    get-aduser -Property ScriptPath -Filter {ScriptPath -notlike $false} | Set-ADUser -ScriptPath $null
+
+# Disable offline files
+& "sc config CscService start=disabled"
+new-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Csc\Parameters" -name FormatDatabase -propertytype DWORD -value 1
