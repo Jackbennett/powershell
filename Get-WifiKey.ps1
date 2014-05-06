220f0bebe0367726c7bdfae92c292edf98c6e916
Jack Bennett <me@jackben.net>
2014-05-06 16:05:16 +0100

    wrapped the md5 hash in a function and written some help on how to use it. note: This is not about make a secure OR secret password. It's just to make a rotating guest wifi key less guassable than the name of the school.

--- Get-WifiKey.ps1
+++ Get-WifiKey.ps1
@@ -1,13 +1,48 @@
-﻿$Input = 'september'
+﻿<#
+.Synopsis
+    Compute a decent wifi key from a memorable string
+.DESCRIPTION
+    This functions is to consistently get a kind of good password
+    that's always the same when given the same hint. This is so administrators can remember 
+    an easy keyword to pull up the password to hand out to guests quickly but avoiding a
+    simple to guess password.
+.EXAMPLE
+    Get-WifiKey september
+    110cea74c
+#>
+function Get-WifiKey
+{
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        # What to use as a hint for the key
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        [string]$hint
 
-$Algorithm = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+        , # The length of the returned key
+        [ValidateRange(1,31)]
+        [int]$length = 9
+    )
 
-$Text = New-Object -TypeName System.Text.UTF8Encoding
+    Begin
+    {
+        $Algorithm = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+        $Text = New-Object -TypeName System.Text.UTF8Encoding
 
-$Key = [System.BitConverter]::ToString(
-    $Algorithm.ComputeHash(
-        $text.GetBytes( $Input )
-        )
-    ).replace('-','').remove(9).ToLower()
+    }
+    Process
+    {
+        $Key = [System.BitConverter]::ToString(
+        $Algorithm.ComputeHash(
+            $text.GetBytes( $hint )
+            )
+        ).replace('-','').remove($length).ToLower()
 
-$Key
+    }
+    End
+    {
+        Write-Output $Key
+    }
+}
