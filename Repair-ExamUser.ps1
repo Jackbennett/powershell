a4926dea082f3b0c4be88a6f9f5fdc5b904303fd
Jack Bennett <me@jackben.net>
2014-05-06 16:38:25 +0100

    adding some help and a NOTES section to propise future work on the cmdlet

--- Repair-ExamUser.ps1
+++ Repair-ExamUser.ps1
@@ -1,3 +1,16 @@
+<#
+.Synopsis
+    Batch clear then copy boilerplate documents into exam accounts.
+.NOTES
+    Possible additions
+
+    * Move the exsiting files to an archive isntead of deletion
+    * Batch reset the exam account password
+    * Set a script to run on first login to GUI prompt for candidate number and name
+        * Create a file of info to link the current state of the folder to a user
+        * Use details to pre-populate the office template headers so students can't forget it.
+        * if number changes lock folder permissions of old details and start a new folder.
+#>
 $source = @'
 \\upholland.lancsngfl.ac.uk\usershares\PupilShared\examination share\Unit 9 Using ICT in Business Teachers' Notes Files\*
 '@
