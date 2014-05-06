a8e42b9ad41d197b9246377891acd937d685f0f2
Jack Bennett <me@jackben.net>
2014-05-06 16:27:22 +0100

    amending the help to clarify how the cmdlet works

--- Exit-Sims.ps1
+++ Exit-Sims.ps1
@@ -1,15 +1,21 @@
 ﻿<#
+.Synopsis
+    Query if a specified computer has SIMS.net running.
 .Description
+    Force close any instance of the SIMS process running on a target computer.
+    This will drop any work currently open. Intended for use when users have left
+    the console open and gone home. This prevents SIMS updates. 
+.NOTES
     Caveats;
     1. Can't remote onto computers you are logged onto already
     2. Warning Messages for each computer that hasn't worked. 
         Will continue through the whole list regardless
 .EXAMPLE
-    .\Exit-Sims.ps1 -ComputerName tech-03,tech-01
+    Exit-SIMS -ComputerName tech-03,tech-01
     @{computerName=tech-03}
     Have SIMS running. Use -Exit to close these
 .EXAMPLE
-    .\Exit-Sims.ps1 -ComputerName tech-03,localhost,tech-01 -Exit
+    Exit-SIMS -ComputerName tech-03,localhost,tech-01 -Exit
 #>
 Param(
     [string[]]$ComputerName = 'localhost'
