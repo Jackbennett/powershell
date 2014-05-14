12a31a70759fcaee4ecce3a76e75a605b0ebb17f
Jack Bennett <me@jackben.net>
2014-05-14 15:26:37 +0100

    New submodule for simple UI interaction in powershell scripts. The exam script uses this to prompt for user credentials

--- exams/Get-CandidateData.ps1
+++ exams/Get-CandidateData.ps1
@@ -1,45 +1,25 @@
-﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+﻿Import-module ShowUI
 
-$objForm = New-Object System.Windows.Forms.Form 
-$objForm.Text = "Candidate Information"
-$objForm.Size = New-Object System.Drawing.Size(300,200) 
-$objForm.StartPosition = "CenterScreen"
+$Candidate = UniformGrid -ControlName "Candidate Information" -Columns 2 {
+    New-Label "Full Name"
+    New-textBox -name "name"
+    New-Label "Candidate Number"
+    New-TextBox -name "number" -MaxLength 5
+    New-Label " "
+    New-Button -Content "Begin Exam" -On_Click {
+        Get-ParentControl |
+            Set-UIValue -passThru |
+            Close-Control
+    }
+} -show
 
-$objForm.KeyPreview = $True
-$objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") 
-    {$x=$objTextBox.Text;$objForm.Close()}})
-$objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") 
-    {$objForm.Close()}})
+$ExamText = @"
+Examination information
+Title: $($exam.title)
 
-$OKButton = New-Object System.Windows.Forms.Button
-$OKButton.Location = New-Object System.Drawing.Size(75,120)
-$OKButton.Size = New-Object System.Drawing.Size(75,23)
-$OKButton.Text = "OK"
-$OKButton.Add_Click({$x=$objTextBox.Text;$objForm.Close()})
-$objForm.Controls.Add($OKButton)
+Candidate Name: $($Candidate.name)
+Candidate Number: $($Candidate.number)
 
-$objLabel = New-Object System.Windows.Forms.Label
-$objLabel.Location = New-Object System.Drawing.Size(10,20) 
-$objLabel.Size = New-Object System.Drawing.Size(280,20) 
-$objLabel.Text = "Full Name"
-$objForm.Controls.Add($objLabel) 
+"@
 
-$objLabel = New-Object System.Windows.Forms.Label
-$objLabel.Location = New-Object System.Drawing.Size(10,60) 
-$objLabel.Size = New-Object System.Drawing.Size(280,20) 
-$objLabel.Text = "Please enter your candidate number"
-$objForm.Controls.Add($objLabel) 
-
-$objTextBox = New-Object System.Windows.Forms.TextBox 
-$objTextBox.Location = New-Object System.Drawing.Size(10,40) 
-$objTextBox.Size = New-Object System.Drawing.Size(260,20) 
-#$objForm.Controls.Add($objTextBox) 
-
-
-$objForm.Topmost = $True
-
-$objForm.Add_Shown({$objForm.Activate()})
-[void] $objForm.ShowDialog()
-
-$x
+write-output $ExamText
