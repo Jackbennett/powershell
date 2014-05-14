7af18ea3a9b3216337f13d642973a441a0fa77fd
Jack Bennett <me@jackben.net>
2014-05-14 14:49:42 +0100

    Complete set of help and examples to go with the cmdlet

--- util/Get-Memory.ps1
+++ util/Get-Memory.ps1
@@ -1,10 +1,36 @@
 ﻿<#
 .Synopsis
-   Show the memory configuration
+    Show the memory configuration
 .DESCRIPTION
-   Use a WMI query to show the current memory configuration of the target computer
+    Use a WMI query to show the current memory configuration of the target computer
 .EXAMPLE
-   Example of how to use this cmdlet
+    Get-Memory
+
+    Computer Name : TECH-02
+    Manufacturer  : Corsair
+    Speed (MHz)   : 1600
+    Capacity (MB) : 4096
+    DeviceLocator : DIMM 1
+    Memory Type   : DDR 3
+
+    Get a list of the memory configuration of the `localhost` computer
+.EXAMPLE
+    get-memory -ComputerName 50-02 | format-table -autosize
+
+    Computer Name Manufacturer                     Speed (MHz) Capacity (MB) DeviceLocator Memory Type
+    ------------- ------------                     ----------- ------------- ------------- -----------
+    50-02         JEDEC ID:7F 7F FE 00 00 00 00 00         667          1024 XMM2          DDR-2
+    50-02         JEDEC ID:7F 7F 7F 0B 00 00 00 00         667          1024 XMM4          DDR-2
+
+    Get the memory configuration of a remote computer formatted in an easy to read way
+.EXAMPLE
+    get-memory | select 'computer name','capacity (MB)'
+
+    Computer Name Capacity (MB)
+    ------------- -------------
+    TECH-02                4096
+
+    Select specific fields from the dataset. Number of fields 4 or below automatically formats as a table.
 #>
 function Get-Memory
 {
