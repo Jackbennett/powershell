be9b8f2c807be80c17c3da450f6596576e7f7253
Jack Bennett <me@jackben.net>
2014-05-23 15:10:29 +0100

    Require: Graphviz installed in path. Applcation to make graph diagrams with powershell and some example uses

---
+++ graphviz-example-1.ps1
@@ -0,0 +1,14 @@
+﻿. ps:\graphviz.ps1
+
+New-Graph G {
+
+    Get-ChildItem -Directory -Recurse -Exclude "*node_modules*"|
+      select name,FullName,parent |
+      where {$_.FullName -notlike "*node_modules*"} |
+      ForEach {
+          Add-Edge $_.parent.toString() $_.name.toString()
+      }
+
+} | dot -Tsvg -o .\ps.svg
+
+.\ps.svg
---
+++ graphviz-example-2.ps1
@@ -0,0 +1,10 @@
+﻿. ps:\graphviz.ps1
+
+New-Graph G {
+
+    Add-Edge "Hyper-A" "vapp-02"
+    Add-Edge "Hyper-A" "vapp-03"
+
+} | dot -Tsvg -o .\ps.svg
+
+.\ps.svg
