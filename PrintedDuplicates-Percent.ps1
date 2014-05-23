16e558a16e9ca083d97fc039cb5b62836b316aa3
Jack Bennett <me@jackben.net>
2014-05-23 15:09:30 +0100

    How to process a report form papercut to get the Percent of duplicate jobs prinded in the report

---
+++ PrintedDuplicates-Percent.ps1
@@ -0,0 +1,10 @@
+﻿$printing = Import-csv .\r36-Apr-25--May-23.csv | where "Printer Name" -Like "brother hl-2250dn room 36" | where "Printed" -eq "Y"
+
+$Sorted = $printing | Group-Object -Property "Username","Document" | where "count" -GT 1
+$SortedValues = $Sorted | measure-object -Property "count" -sum
+
+
+$Jobs = $printing.count
+$dupe = $SortedValues.Sum - $SortedValues.Count
+
+$dupe / $Jobs * 100
