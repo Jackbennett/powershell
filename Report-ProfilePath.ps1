bbe1f834bd025b392fcf253b9110c00014421473
Jack Bennett <me@jackben.net>
2014-06-09 12:15:18 +0100

    correct use of the powershell pipeline to contrain the objects. should really learn how to do this with the filter string in the AD for perfromance reasons

--- Report-ProfilePath.ps1
+++ Report-ProfilePath.ps1
@@ -5,15 +5,28 @@
    If any profile path is set, does it also contains the users account name.
    Troubleshooting an issue where users seems to randomly get another users profiles set for them at logoff.
 .EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
+    Report-ProfilePath
+    Got 1371 users
+    Problems on:
+
+    SamAccountName      ProfilePath
+    --------------      -----------
+    07test              \\uhserver1\users$\08test\profile.v2
 #>
 function Report-ProfilePath
 {
     Process
     {
-        get-aduser -filter * -properties "SamAccountName","ProfilePath" | 
-        where {$psitem.profilePath -notmatch $psitem.samAccontName}
+        $result = get-aduser -filter * -properties "SamAccountName","ProfilePath" -OutVariable users |
+            where {($psitem.profilePath -ne $null) -and ($psitem.ProfilePath -notmatch $psitem.SamAccountName)}
+    }
+    End{
+        Write-Output "Got $($users.count) users"
+        if($result){
+            Write-Output "Problems on:"
+            Write-Output $result | select SamAccountName, ProfilePath
+        } else {
+            Write-Output "No incorrect profiles found"
+        }
     }
 }
