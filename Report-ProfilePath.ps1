e3bcd3d1e418624ee3aa5f196a5adf0d87ddc486
Jack Bennett <me@jackben.net>
2014-06-09 09:22:47 +0100

    Making a report script to monitor the network

---
+++ Report-ProfilePath.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.Synopsis
+   Do all the eactive directory accounts have the correct profile path
+.DESCRIPTION
+   If any profile path is set, does it also contains the users account name.
+   Troubleshooting an issue where users seems to randomly get another users profiles set for them at logoff.
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Report-ProfilePath
+{
+    Process
+    {
+        get-aduser -filter * -properties "SamAccountName","ProfilePath" | 
+        where {$psitem.profilePath -notmatch $psitem.samAccontName}
+    }
+}
