34fe023ccc6949a92b3a5ea1ab0f410beaf20548
Jack Bennett <me@jackben.net>
2014-06-24 15:12:43 +0100

    working on the configure script to enable jumbo packets on target machines, currently declawed i.e. damaging code comented out.

---
+++ JumboPacket.ps1
@@ -0,0 +1,46 @@
+﻿<#
+.Synopsis
+   Enable Jumbo Packets
+.DESCRIPTION
+   enable the use of Jumbo Packets on all ethernet adapters
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Enable-JumboPacket
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Target Computer
+        [Parameter(ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Computername
+    )
+
+    Begin
+    {
+        if($Computername -isnot [Microsoft.Management.Infrastructure.CimSession]){
+            try{
+                $Computername = $Computername | New-CimSession
+            } catch {
+                throw
+                exit
+            }
+        }
+    }
+    Process
+    {
+        $Computername | Get-NetAdapterAdvancedProperty -DisplayName “Jumbo Packet” 
+
+           # Set-NetAdapterAdvancedProperty -RegistryValue '9014' -NoRestart
+    }
+    End
+    {
+        Write-Output "Target computer should be restarted."
+
+        $Computername | Remove-CimSession
+    }
+}
