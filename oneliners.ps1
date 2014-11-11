0d16473e63e428369d6edb9ccfe8cae7ee9751f1
Jack Bennett <me@jackben.net>
2014-11-11 09:28:08 +0000

    adding to the collection of one line scripts I've quickl;y made to run.

--- oneliners.ps1
+++ oneliners.ps1
@@ -28,3 +28,36 @@ invoke-command -Session $computers -ScriptBlock {
     Import-Module "\\uhserver1\HomeStaff\Home\j.bennett\ps\Application\Application.psm1"
     Get-Application -name Logicator
 } | select computername,name,version | format-table -AutoSize | out-file Output -Append
+
+# Replace all placeholder values with the actual username for H drives.
+# Odd problem with year 7
+Get-ADGroupMember -Identity "Intake 2014" | 
+    Get-ADuser -Properties "HomeDirectory" |
+    Where-Object HomeDirectory -Match "%username%" |
+    ForEach-Object {
+        Set-ADuser -Identity $_ -HomeDirectory ($_.homeDirectory -replace "%username%", $_.samAccountName)
+   }
+
+# Test if powershell remoting is enabled on a selection of computers
+Import-module ActiveDirectory
+$computer = get-ADComputer -Filter * -searchBase "OU=R50,OU=Computers,OU=Upholland,DC=upholland,DC=lancsngfl,DC=ac,DC=uk"
+
+foreach($comp in $computer){
+    invoke-command -ComputerName $comp.name -ScriptBlock {
+        get-service Netlogon | select-object Displayname,status
+    }
+}
+
+Remove-Variable computer
+
+# wipe the local profiles of selected user groups on target machines
+import-module util
+$s = New-ComputerList -Room 37 -Computer (2..32) | New-PSSession
+
+invoke-command -Session $s -ScriptBlock {
+    get-childitem -Directory "C:/Users" -Include "14*" | Remove-Item -Force
+}
+
+# reset a whole year group password
+Get-ADGroupMember -Identity "Intake 2014" |
+    Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "password" -Force)
