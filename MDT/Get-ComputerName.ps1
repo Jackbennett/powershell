acb5f6fefedd9bc120fca75b6234da5faed9b3b5
Jack Bennett <me@jackben.net>
2014-11-18 15:23:55 +0000

    Script now asks for the hostname but the gui doesn't get infront of mdt. needs more work

--- MDT/Get-ComputerName.ps1
+++ MDT/Get-ComputerName.ps1
@@ -1,24 +1,51 @@
 # Demo $Mac =  "00:21:CC:67:F0:0C"
+# tech-02 $MAC = "D4:3D:7E:90:E8:ED"
 # Property ComputerName is depreciated. Use OSDComputerName
 
 $URI = "http://tech-02:5984/macs/"
+$MAC = $TSenv:MACADDRESS001.ToUpper()
 
-$MAC = $TSenv:MACADDRESS001
-$MAC = $MAC.ToUpper()
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') > $null
+$message = @"
+Hostname not found
+Set a new name for this MAC address:
+$MAC
+"@
 
-$record = Invoke-WebRequest -Uri ($URI + $MAC) -Method Get -UseBasicParsing
-if($record.StatusCode -in 200, 304 ){
-    Write-Output "Success: $($record.StatusCode)"
-    $record | Add-Member -Value (ConvertFrom-json $record.Content) -Name Body -MemberType NoteProperty
+try {
+    $doc = Invoke-WebRequest -Uri ($URI + $MAC) -Method Get -UseBasicParsing
+} catch {
+    # Close the TS UI temporarily
+    $TSProgressUI = New-Object -COMObject Microsoft.SMS.TSProgressUI
+    $TSProgressUI.CloseProgressDialog()
+
+    $ComputerName = [Microsoft.VisualBasic.Interaction]::InputBox($message, "Set Computer Name", "Test-01")
+    if($ComputerName -notmatch "test-*"){
+        $doc = Invoke-WebRequest -UseBasicParsing -Uri ($URI) -Method Post -ContentType 'application/json' -Body (
+            Convertto-json @{
+                "_id" = $MAC
+                "hostname" = $ComputerName
+            }
+        )
+    }
+}
+
+if($doc.StatusCode -in 200, 304 ){
+    Write-Output "Success: $($doc.StatusCode)"
+    $ComputerName = (Convertfrom-json $doc.Content).hostname
+} elseif($doc.StatusCode -eq 201){
+    Write-Output "updated database"
+} elseif($doc.StatusCode -eq 404){
+    Write-Output "missing"
 } else {
-    Write-Output "Failure: $($record.StatusCode)"
+    Write-Output "Failure: $($doc.StatusCode)"
     Exit 1
 }
 
-if($record.Body.Hostname){
-    Set-Item TSenv:OSDCOMPUTERNAME -Value $record.Body.hostname
+if($ComputerName){
+    Set-Item TSenv:OSDCOMPUTERNAME -Value $ComputerName
 } else {
     Write-Output 'Hostname for MAC not present'
 }
 
-Write-Output "New Environment name: $TSenv:OSDCOMPUTERNAME"
+Write-Output "New name: $ComputerName"
