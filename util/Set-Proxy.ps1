84ec751d3bf603530112e908b546b83b9cd41f6d
Jack Bennett <jbennett@birkdalehigh.co.uk>
2015-01-13 13:07:52 +0000

    refactor to use function calls for setting the proxy. also adding the SSID param to only detect the network proxy settings when given a ssid to check for

--- util/Set-Proxy.ps1
+++ util/Set-Proxy.ps1
@@ -17,43 +17,60 @@ function Set-Proxy
         # Param1 help description
         [Parameter(ValueFromPipelineByPropertyName=$true,
                    Position=0)]
-        $proxy = "http://proxy.lancsngfl.ac.uk:8080"
+        $proxy = "http://10.201.0.27:801"
 
         , # If you want to permenantly set this. reqires restart
         [switch]
         $global
+
+        , # SSID to automatically detect
+        [string]
+        $SSID
     )
 
     Begin
     {
-    }
-    Process
-    {
-        Get-SSID | select ssid | foreach-object {
-	        if ($_.ssid -match "upholland"){
-                if($global){
-                    [environment]::SetEnvironmentVariable("http_proxy", $proxy, "User")
-                    [environment]::SetEnvironmentVariable("https_proxy", $proxy, "User")
-                } else {
-                    $env:http_proxy = $proxy
-                    $env:https_proxy = $proxy
-                }
-		        git config --global --add http.proxy $proxy
-		        git config --global --add https.proxy $proxy
-	        } else {
-                if($global){
-                    [environment]::SetEnvironmentVariable("http_proxy", $null, "User")
-                    [environment]::SetEnvironmentVariable("https_proxy", $null, "User")
-                } else {
-                    Remove-Variable $env:http_proxy
-                    Remove-Variable $env:https_proxy
-                }
-		        git config --global --remove-section http
-		        git config --global --remove-section https
-	        }
+        function setProxyConfiguration
+        {
+            if($global){
+                [environment]::SetEnvironmentVariable("http_proxy", $proxy, "User")
+                [environment]::SetEnvironmentVariable("https_proxy", $proxy, "User")
+            } else {
+                $env:http_proxy = $proxy
+                $env:https_proxy = $proxy
+            }
+		    git config --global --add http.proxy $proxy
+		    git config --global --add https.proxy $proxy
+        }
+
+        function removeProxyConfiguration
+        {
+            if($global){
+                [environment]::SetEnvironmentVariable("http_proxy", $null, "User")
+                [environment]::SetEnvironmentVariable("https_proxy", $null, "User")
+            } else {
+                Remove-Item env:http_proxy
+                Remove-Item env:https_proxy
+            }
+		    git config --global --remove-section http
+		    git config --global --remove-section https
         }
     }
-    End
+
+    Process
     {
+        if($SSID){
+            Get-SSID | select ssid | foreach-object {
+	            if ($_.ssid -match $SSID){
+                    setProxyConfiguration
+	            } else {
+                    removeProxyConfiguration
+	            }
+            }
+            return
+        }
+
+        setProxyConfiguration
+
     }
 }
