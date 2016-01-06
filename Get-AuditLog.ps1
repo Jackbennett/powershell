51585b4b54502f9d5e1abf4fed32d0afc3d020c6
Mr J Bennett <jbennett@birkdalehigh.co.uk>
2016-01-06 11:48:32 +0000

    Specify event dates for today. Slight code style tidy up.

--- Get-AuditLog.ps1
+++ Get-AuditLog.ps1
@@ -1,7 +1,9 @@
 ﻿$server = "bhs-fs02"
-$out = (new-item -ItemType File -Path 'C:\Temp\4663Events.csv').AppendText()
-$out.WriteLine("ServerName,EventID,TimeCreated,UserName,File_or_Folder,AccessMask")
+$outStream = (new-item -ItemType File -Path "C:\Temp\4663Events-$((get-date -Format s).Replace(':', '.')).csv").AppendText()
 $ns = @{e = "http://schemas.microsoft.com/win/2004/08/events/event"}
+$StartDate = Get-Date
+$EndDate = $StartDate.AddDays(1)
+
 $AccessMaskLookup = @{
  0x1     = 'Read Data / List Directory'
  0x2     = 'Write Data / Add File'
@@ -11,12 +13,17 @@ $AccessMaskLookup = @{
  0x100   = 'Change file Attributes'
  0x10000 = 'Delete'
 }
+$outStream.WriteLine("ServerName,EventID,TimeCreated,UserName,File_or_Folder,AccessMask")
 
-foreach ($svr in $server)
-    {    $evts = Get-WinEvent -computer $svr -FilterHashtable @{logname="security";id="4663"} -oldest
+foreach ($svr in $server){
+    $evts = Get-WinEvent -computer $svr -Oldest -FilterHashtable @{
+        logname = "security";
+        id = "4663";
+        StartTime = $StartDate.toShortDateString();
+        EndTime = $EndDate.toShortDateString();
+    }
 
-    foreach($evt in $evts)
-        {
+    foreach($evt in $evts){
         $xml = [xml]$evt.ToXml()
 
         $SubjectUserName = Select-Xml -Xml $xml -Namespace $ns -XPath "//e:Data[@Name='SubjectUserName']/text()" | Select-Object -ExpandProperty Node | Select-Object -ExpandProperty Value
@@ -29,12 +36,12 @@ foreach ($svr in $server)
             $AccessMask = $AccessMaskLookup[[convert]::ToInt32($AccessMask,16)]
         }
 
-        $out.WriteLine("$($svr),$($evt.id),$($evt.TimeCreated),$SubjectUserName,$ObjectName, $AccessMask")
+        $outStream.WriteLine("$($svr),$($evt.id),$($evt.TimeCreated),$SubjectUserName,$ObjectName,$AccessMask")
 
         Write-Verbose $svr
         Write-Verbose "$($evt.id), $($evt.TimeCreated), $SubjectUserName, $ObjectName, $AccessMask"
 
-        }
     }
+}
 
-$out.close()
+$outStream.close()
