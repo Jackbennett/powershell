76d43977ec889bd27804bb4620905c6886138666
JackBennett <me@jackben.net>
2017-10-09 22:18:38 +0100

    reformat code

--- util/Set-Proxy.ps1
+++ util/Set-Proxy.ps1
@@ -8,15 +8,14 @@
 .EXAMPLE
    Another example of how to use this cmdlet
 #>
-function Set-Proxy
-{
+function Set-Proxy {
     [CmdletBinding()]
     [OutputType([int])]
     Param
     (
         # Param1 help description
-        [Parameter(ValueFromPipelineByPropertyName=$true,
-                   Position=0)]
+        [Parameter(ValueFromPipelineByPropertyName = $true,
+            Position = 0)]
         $proxy = "http://10.201.0.27:801"
 
         , # If you want to permenantly set this. reqires restart
@@ -28,44 +27,51 @@ function Set-Proxy
         $SSID
     )
 
-    Begin
-    {
-        function setProxyConfiguration
-        {
-            if($global){
+    Begin {
+        $RegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+
+        function setProxyConfiguration {
+            if ($global) {
                 [environment]::SetEnvironmentVariable("http_proxy", $proxy, "User")
                 [environment]::SetEnvironmentVariable("https_proxy", $proxy, "User")
-            } else {
+            }
+            else {
                 $env:http_proxy = $proxy
                 $env:https_proxy = $proxy
             }
-		    git config --global --add http.proxy $proxy
-		    git config --global --add https.proxy $proxy
+
+            git config --global --add http.proxy $proxy
+            git config --global --add https.proxy $proxy
+            
+            Set-ItemProperty -Path $RegistryPath -Name ProxyServer -Value $proxy
+            Set-ItemProperty -Path $RegistryPath -Name ProxyEnable -Value $true
+
         }
 
-        function removeProxyConfiguration
-        {
-            if($global){
+        function removeProxyConfiguration {
+            if ($global) {
                 [environment]::SetEnvironmentVariable("http_proxy", $null, "User")
                 [environment]::SetEnvironmentVariable("https_proxy", $null, "User")
-            } else {
+            }
+            else {
                 Remove-Item env:http_proxy
                 Remove-Item env:https_proxy
             }
-		    git config --global --remove-section http
-		    git config --global --remove-section https
+            git config --global --remove-section http
+            git config --global --remove-section https
+            
+            Set-ItemProperty -Path $RegistryPath -Name ProxyEnable -Value $false
         }
 
-        function notifyAllWindows
-        {
+        function notifyAllWindows {
             #This I don't understand one iota. What's with the numbers?
             $method = 'SendMessageTimeout_6a057adb_f92f_4e34_bda2_42bc79d05f5c'
             $type = $method -as [type]
 
-            if($global){
-                if(-not $type){
+            if ($global) {
+                if (-not $type) {
                     #import sendmessagetimeout from win32
-$type = Add-Type -pass -Name $method -MemberDefinition @"
+                    $type = Add-Type -pass -Name $method -MemberDefinition @"
 [DllImport("user32", SetLastError = true)]
 public static extern IntPtr SendMessageTimeout(
     IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
@@ -91,22 +97,22 @@ public static extern IntPtr SendMessageTimeout(
         }
     }
 
-    Process
-    {
-        if($SSID){
+    Process {
+        if ($SSID) {
             Get-SSID | select ssid | foreach-object {
-	            if ($_.ssid -match $SSID){
+                if ($_.ssid -match $SSID) {
                     setProxyConfiguration
-	            } else {
+                }
+                else {
                     removeProxyConfiguration
-	            }
+                }
             }
             return
         }
 
         setProxyConfiguration
 
-        if($global){
+        if ($global) {
             notifyAllWindows
         }
 
